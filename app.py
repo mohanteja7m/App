e805dfa240fbe5b208078e176c0007420f3141b7
mohanteja7m <144516072+mohanteja7m@users.noreply.github.com>
2023-09-09 16:49:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ Markowitz_log_ret = np.log(dataset / dataset.shift(1))
 mean_log_returns = Markowitz_log_ret.mean().values
 # Calculate expected return (weighted sum of mean returns)
 Markowitz_exp_ret = mean_log_returns.dot(weights_array) * 252
-print(f"\nExpected return of the portfolio is :", Markowitz_exp_ret)
+print(f'\nExpected return of the portfolio is : {Markowitz_exp_ret[0]}')
 
 # Calculate expected volatility (risk)
 Markowitz_exp_vol = np.sqrt(weights_array.T.dot(252 * Markowitz_log_ret.cov().dot(weights_array)))
